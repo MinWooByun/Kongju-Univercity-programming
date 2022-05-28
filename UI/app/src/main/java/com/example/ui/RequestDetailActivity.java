e0034f98c253d4f9f0ca071b142fcc6d0bf2720a
leech714 <koh3621603@naver.com>
2022-05-29 01:15:25 +0900

    0529 KWH
    intent값 넘기기 추가
     -noticeBoardActivity
     -RequestDetailActivity
    
    type별 버튼 설정
     -RequestDetailActivity
     -noticeboardActivity
    
    LoginActivity 수정
     signupActivity->noticeBoardActivity
     ID -> u_id

--- UI/app/src/main/java/com/example/ui/RequestDetailActivity.java
+++ UI/app/src/main/java/com/example/ui/RequestDetailActivity.java
@@ -2,7 +2,10 @@ package com.example.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RequestDetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class RequestDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.request_detail);
+
+        Intent intent = getIntent();;
+        String u_id= intent.getExtras().getString("u_id");
+        int type = intent.getExtras().getInt("type");
+
+        Button btnProposal = (Button) findViewById(R.id.btnProposal);
+
+        if(type!=1)
+            btnProposal.setVisibility(View.GONE);
+
+        btnProposal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RequestDetailActivity.this ,Proposal_Suggest.class);
+                startActivity(intent);
+                intent.putExtra("uId", u_id);
+            }
+        });
     }
 }
