0e06d14386afbdfc11a4e677a06cc9ebe34fcf77
leech714 <koh3621603@naver.com>
2022-05-30 18:25:55 +0900

    0530 김원호
    카테고리, 검색 구현
    로그아웃 구현
    제목 리스트 뷰 이상하던 크기 변경

--- UI/app/src/main/java/com/example/ui/dbHelper.java
+++ UI/app/src/main/java/com/example/ui/dbHelper.java
@@ -38,6 +38,50 @@ public class dbHelper extends SQLiteOpenHelper {
         return titles;
     }
 
+    //카테고리로 제목들 가져오기
+    public ArrayList<ListItem> getTitles_category(int position, int max_position, String u_id){
+        StringBuffer sb = new StringBuffer();
+        if(position != max_position){
+            sb.append("SELECT number, title FROM repairRequestTable WHERE object = '" + position +"'");
+        }
+        else
+            sb.append("SELECT number, title FROM repairRequestTable WHERE userId = '" + u_id +"'");
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(sb.toString(), null);
+
+        ArrayList<ListItem> titles = new ArrayList<ListItem>();
+        String title = null;
+        while(cursor.moveToNext()){
+            ListItem item = new ListItem(cursor.getInt(0), cursor.getString(1));
+            titles.add(item);
+        }
+        return titles;
+    }
+
+    //검색으로 제목 가져오기
+    public ArrayList<ListItem> getTitles_search(int position, String search_text){
+        StringBuffer sb = new StringBuffer();
+
+        if(position!=0)
+            sb.append("SELECT number, title FROM repairRequestTable WHERE title LIKE '%" + search_text +"%' AND object = '" + position + "'");
+        else
+            sb.append("SELECT number, title FROM repairRequestTable WHERE title LIKE '%" + search_text +"%'");
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(sb.toString(), null);
+
+        ArrayList<ListItem> titles = new ArrayList<ListItem>();
+        String title = null;
+        while(cursor.moveToNext()){
+            ListItem item = new ListItem(cursor.getInt(0), cursor.getString(1));
+            titles.add(item);
+        }
+        return titles;
+    }
+
     public String getRequest(int number){
         StringBuffer sb = new StringBuffer();
         sb.append("SELECT title, symptom, symptom_contents, object FROM repairRequestTable WHERE number = '" + number + "'");
@@ -54,7 +98,7 @@ public class dbHelper extends SQLiteOpenHelper {
 
     public void insertRequest(SQLiteDatabase db, String u_id,  String title, int symptom, String symptom_contents, int object){
         Log.v("symptom:", String.valueOf(symptom));
-        String sql = "INSERT INTO repairRequestTable(userId,title,object,symptom,symptom_contents) VALUES"+"("+"'"+u_id+"'"+","+"'"+title+"'"+","+object+","+symptom+","+"'"+symptom_contents+"'"+");";
+        String sql = "INSERT INTO repairRequestTable(u_id,title,object,symptom,symptom_contents) VALUES"+"("+"'"+u_id+"'"+","+"'"+title+"'"+","+object+","+symptom+","+"'"+symptom_contents+"'"+");";
         db.execSQL(sql);
     }
 
