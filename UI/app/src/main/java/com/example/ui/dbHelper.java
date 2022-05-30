a82f51d4ed033f101ac7461ac4f9b99eb0522f75
MinWooByun <yja05092@gmail.com>
2022-05-30 21:31:32 +0900

    main이랑 맞추기

--- UI/app/src/main/java/com/example/ui/dbHelper.java
+++ UI/app/src/main/java/com/example/ui/dbHelper.java
@@ -41,6 +41,70 @@ public class dbHelper extends SQLiteOpenHelper {
         return titles;
     }
 
+    //카테고리로 제목들 가져오기
+    public ArrayList<ListItem> getTitles_category(int position, int max_position, String u_id){
+        StringBuffer sb = new StringBuffer();
+        if(position != max_position){
+            sb.append("SELECT number, title FROM repairRequestTable WHERE object = '" + position +"'");
+        }
+        else
+            sb.append("SELECT number, title FROM repairRequestTable WHERE userId = '" + u_id +"'");
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(sb.toString(), null);
+
+        ArrayList<ListItem> titles = new ArrayList<ListItem>();
+        String title = null;
+        while(cursor.moveToNext()){
+            ListItem item = new ListItem(cursor.getInt(0), cursor.getString(1));
+            titles.add(item);
+        }
+        return titles;
+    }
+
+    //검색으로 제목 가져오기
+    public ArrayList<ListItem> getTitles_search(int position, String search_text){
+        StringBuffer sb = new StringBuffer();
+
+        if(position!=0)
+            sb.append("SELECT number, title FROM repairRequestTable WHERE title LIKE '%" + search_text +"%' AND object = '" + position + "'");
+        else
+            sb.append("SELECT number, title FROM repairRequestTable WHERE title LIKE '%" + search_text +"%'");
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(sb.toString(), null);
+
+        ArrayList<ListItem> titles = new ArrayList<ListItem>();
+        String title = null;
+        while(cursor.moveToNext()){
+            ListItem item = new ListItem(cursor.getInt(0), cursor.getString(1));
+            titles.add(item);
+        }
+        return titles;
+    }
+
+    public String getRequest(int number){
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT title, symptom, symptom_contents, object FROM repairRequestTable WHERE number = '" + number + "'");
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        String result = "";
+        Cursor cursor = db.rawQuery(sb.toString(), null);
+        while(cursor.moveToNext()){
+            result += cursor.getString(0) + "##,#" + cursor.getInt(1) + "##,#" + cursor.getString(2) + "##,#" + cursor.getInt(3);
+        }
+        return result;
+    }
+
+    public void insertRequest(SQLiteDatabase db, String u_id,  String title, int symptom, String symptom_contents, int object){
+        Log.v("symptom:", String.valueOf(symptom));
+        String sql = "INSERT INTO repairRequestTable(u_id,title,object,symptom,symptom_contents) VALUES"+"("+"'"+u_id+"'"+","+"'"+title+"'"+","+object+","+symptom+","+"'"+symptom_contents+"'"+");";
+        db.execSQL(sql);
+    }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
