bad2c6e62d32ed78720712adb147f0cb37cb0019
MinWooByun <yja05092@gmail.com>
2022-06-06 00:41:27 +0900

    0606 변민우
    만족도 평가 확인 가격도 뜨게 수정함

--- UI/app/src/main/java/com/example/ui/SatisfiedConfirm.java
+++ UI/app/src/main/java/com/example/ui/SatisfiedConfirm.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class SatisfiedConfirm extends Activity {
 
@@ -19,6 +20,7 @@ public class SatisfiedConfirm extends Activity {
         RatingBar ratingBarState = findViewById(R.id.ratingBarState);
         RatingBar ratingBarKindness = findViewById(R.id.ratingBarKindness);
         RatingBar ratingBarTerm = findViewById(R.id.ratingBarTerm);
+        TextView tvPrice = findViewById(R.id.tvPrice);
         Button btnClose = findViewById(R.id.btnClose);
 
         Intent intent = getIntent();
@@ -26,12 +28,13 @@ public class SatisfiedConfirm extends Activity {
 
         dbHelper helper = new dbHelper(this, 1);
         SQLiteDatabase db = helper.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT S_State, S_Kindness, S_Term FROM satisficationTable WHERE p_num = '"+ number +"'",null);
+        Cursor cursor = db.rawQuery("SELECT S_State, S_Kindness, S_Term, price FROM satisficationTable WHERE p_num = '"+ number +"'",null);
 
         while (cursor.moveToNext()) {
             ratingBarState.setRating(cursor.getInt(0));
             ratingBarKindness.setRating(cursor.getInt(1));
             ratingBarTerm.setRating(cursor.getInt(2));
+            tvPrice.setText(String.valueOf(cursor.getInt(3)));
         }
 
         db.close();
